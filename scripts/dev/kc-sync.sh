58dc6e8ff284f01caf5ac221c5e5927dfeb50dd5
Edison Wang <ew040607@gmail.com>
2026-03-06 18:24:18 -0500

    feat!: KC syncing bash script

---
+++ scripts/dev/kc-sync.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# get token
+TOKEN=$(curl -s -X POST http://localhost:8080/realms/master/protocol/openid-connect/token \
+  -d "client_id=admin-cli" \
+  -d "username=mcc-admin" \
+  -d "password=uworbital" \
+  -d "grant_type=password" | python3 -m json.tool | grep access_token | cut -d'"' -f4)
+
+# get realm settings
+curl -s -H "Authorization: Bearer $TOKEN" \
+  http://localhost:8080/admin/realms/mcc \
+  -o /tmp/realm.json
+
+# get clients
+curl -s -H "Authorization: Bearer $TOKEN" \
+  http://localhost:8080/admin/realms/mcc/clients \
+  -o /tmp/clients.json
+
+# get roles
+curl -s -H "Authorization: Bearer $TOKEN" \
+  http://localhost:8080/admin/realms/mcc/roles \
+  -o /tmp/roles.json
+
+# merge them into one file
+python3 -c "
+import json
+realm = json.load(open('/tmp/realm.json'))
+realm['clients'] = json.load(open('/tmp/clients.json'))
+realm['roles'] = {'realm': json.load(open('/tmp/roles.json'))}
+json.dump(realm, open('../../backend/mcc-realm.json', 'w'), indent=2)
+print('Done')
+"
