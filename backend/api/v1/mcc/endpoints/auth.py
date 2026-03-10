abb7a26a82cd844d5e2b360abe5a780b9316addf
Edison Wang <ew040607@gmail.com>
2026-03-09 22:21:03 -0400

    feat: login and callback endpoints

--- backend/api/v1/mcc/endpoints/auth.py
+++ backend/api/v1/mcc/endpoints/auth.py
@@ -1,3 +1,29 @@
+from data.data_wrappers.wrappers import MCCUsersWrapper
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
+from keycloak.client import keycloak
 
 mcc_auth_router = APIRouter(tags=["MCC", "Authentication"])
+
+
+@mcc_auth_router.get("/login")
+def login() -> None:
+    """
+    Login endpoint
+    """
+    print(keycloak.login_url)
+    RedirectResponse(url=keycloak.login_url)
+
+
+@mcc_auth_router.get("/callback")
+def callback(code: str) -> dict[str, str]:
+    """
+    Callback endpoint redirected to by keycloak for tokens
+    """
+    tokens = keycloak.get_tokens(code)
+    user_info = keycloak.decode_id_token(tokens["id_token"])
+    try:
+        MCCUsersWrapper().get_by_id(user_info["sub"])
+    except ValueError:
+        MCCUsersWrapper().create({"id": user_info["sub"], "email": user_info["email"], "phone_number": ""})
+    return {"id_token": tokens["id_token"], "access_token": tokens["access_token"]}
--- backend/keycloak/client.py
+++ backend/keycloak/client.py
@@ -1,7 +1,10 @@
+from typing import Any
 from urllib.parse import urlencode
 
+import httpx
 from config.config import settings
 from config.keycloak_config import KeycloakConfig
+from jose import jwt
 
 
 class KeycloakClient:
@@ -31,7 +34,38 @@ class KeycloakClient:
         """
         Returns keycloak login URL.
         """
-        return f"{self.config.url}/realms/{self.config.realm}/protocol/openid-connect/auth?{self._params}"
+        return f"http://localhost:8080/realms/{self.config.realm}/protocol/openid-connect/auth?{self._params}"
+
+    def get_tokens(self, code: str) -> dict[str, Any]:
+        """
+        Makes API call to keycloak service to get user tokens
+        """
+        with httpx.Client() as client:
+            response = client.post(
+                f"{self.config.url}/realms/{self.config.realm}/protocol/openid-connect/token",
+                data={
+                    "grant_type": "authorization_code",
+                    "client_id": self.config.client_id,
+                    "client_secret": self.config.client_secret,
+                    "code": code,
+                    "redirect_uri": self.config.callback_url,
+                },
+            )
+            return response.json()  # type: ignore[no-any-return]
+
+    def decode_id_token(self, id_token: str) -> dict[str, Any]:
+        """
+        Decodes user id token to get user information
+        """
+        return jwt.decode(  # type: ignore[no-any-return]
+            id_token,
+            key="",
+            options={
+                "verify_signature": False,
+                "verify_aud": False,
+                "verify_at_hash": False,
+            },
+        )
 
 
 keycloak = KeycloakClient(settings.keycloak)
