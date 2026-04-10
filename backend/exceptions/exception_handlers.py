c4488c7a4672cff191dc0121f65e92c0be925c41
cryolins <hepeilin07@gmail.com>
2026-04-10 13:07:18 -0400

    pytests for exception handlers

--- backend/exceptions/exception_handlers.py
+++ backend/exceptions/exception_handlers.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 
-from exceptions import (
+from exceptions.exceptions import (
     BaseOrbitalError,
     DatabaseError,
     InvalidArgumentError,
