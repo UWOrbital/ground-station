6df4d29bd908ebe15d00ec0f8586557f5b131599
Cameron Lee <cameronkmlee10@gmail.com>
2026-06-03 18:26:47 -0400

    Add ESLint and Prettier to ARO

--- frontend/mcc/src/components/Nav.tsx
+++ frontend/mcc/src/components/Nav.tsx
@@ -9,7 +9,7 @@ import { useTheme } from "../contexts/ThemeContext";
  */
 function Nav() {
   // TODO: create user auth that checks if the user is logged in
-  const isLoggedIn = false;
+  const isLoggedIn = true;
   const { theme, toggleTheme } = useTheme();
 
   return (
