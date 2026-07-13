b19c389dce589584a40d4b498e8ecc6061feee5b
Eric N <120667897+ninja888-8@users.noreply.github.com>
2026-07-13 12:35:06 -0400

    created main table endpoints (#95)
    * created main table endpoints (command & telemetry)
    
    * updated responses.py with new schemas
    
    * tweaked backend_setup.py to include main telemetry

--- backend/api/v1/mcc/endpoints/main_commands.py
+++ backend/api/v1/mcc/endpoints/main_commands.py
@@ -1,19 +1,34 @@
 from data.data_wrappers.wrappers import MainCommandWrapper
 from fastapi import APIRouter
-from fastapi_cache.decorator import cache
+from fastapi.exceptions import HTTPException
+from mcc_keycloak.client import keycloak
 
-from api.v1.mcc.models.responses import MainCommandsResponse
+from api.v1.mcc.models.responses import MainCommandResponse, MainCommandsResponse
 
 main_commands_router = APIRouter(tags=["MCC", "Main Commands"])
 
 
-@main_commands_router.get("/")
-@cache(expire=300)  # Cache for 5 minutes
-async def get_main_commands() -> MainCommandsResponse:
+@main_commands_router.get("/", dependencies=[keycloak.require_auth])
+async def get_all_commands() -> MainCommandsResponse:
     """
     Gets the main commands that are available for the MCC
 
-    :return: list of all commands
+    :return: the list of all commands.
     """
     items = MainCommandWrapper().get_all()
     return MainCommandsResponse(data=items)
+
+
+@main_commands_router.get("/{command_id}", dependencies=[keycloak.require_auth])
+async def get_command_by_id(command_id: int) -> MainCommandResponse:
+    """
+    Gets the main command by the id provided
+
+    :param command_id: the ID of the command to retrieve.
+    :return: the matching command.
+    """
+    try:
+        command = MainCommandWrapper().get_by_id(command_id)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail="Command not found") from e
+    return MainCommandResponse(data=command)
