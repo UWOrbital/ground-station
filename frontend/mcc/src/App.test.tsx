e6373347fb10e70567f219aca01e86f7d025a451
Cameron Lee <cameronkmlee10@gmail.com>
2026-07-15 20:46:55 -0400

    Fix test warnings

--- frontend/mcc/src/App.test.tsx
+++ frontend/mcc/src/App.test.tsx
@@ -1,14 +1,18 @@
-import { describe, it } from "vitest";
-import { render } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { render, waitFor, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 describe("App", () => {
-  it("should render without crashing", () => {
+  it("should render without crashing", async () => {
     render(
       <BrowserRouter>
         <App />
       </BrowserRouter>,
     );
+
+    await waitFor(() => {
+      expect(screen.getByRole("navigation")).toBeInTheDocument()
+    })
   });
 });
--- frontend/mcc/src/components/Nav.test.tsx
+++ frontend/mcc/src/components/Nav.test.tsx
@@ -1,17 +1,13 @@
-import { describe, it, expect, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
 import Nav from "./Nav";
 import { AuthProvider } from "../contexts/AuthContext";
 import { ThemeProvider } from "../contexts/ThemeContext";
 
-vi.mock("../utils/api/auth", () => ({
-  checkAuth: vi.fn().mockResolvedValue(false),
-}))
-
 describe("Nav", () => {
-  it("renders logo", () => {
+  it("renders logo", async () => {
     render(
       <AuthProvider>
         <ThemeProvider>
@@ -21,10 +17,13 @@ describe("Nav", () => {
         </ThemeProvider>
       </AuthProvider>,
     );
-    expect(screen.getByAltText("orbital logo")).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByAltText("orbital logo")).toBeInTheDocument();
+    });
   });
 
-  it("renders navigation links", () => {
+  it("renders navigation links", async () => {
     render(
       <AuthProvider>
         <ThemeProvider>
@@ -34,10 +33,13 @@ describe("Nav", () => {
         </ThemeProvider>
       </AuthProvider>,
     );
-    expect(screen.getByText("Dashboard")).toBeInTheDocument();
-    expect(screen.getByText("Commands")).toBeInTheDocument();
-    expect(screen.getByText("ARO Admin")).toBeInTheDocument();
-    expect(screen.getByText("Live Sessions")).toBeInTheDocument();
-    expect(screen.getByText("Login")).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByText("Dashboard")).toBeInTheDocument();
+      expect(screen.getByText("Commands")).toBeInTheDocument();
+      expect(screen.getByText("ARO Admin")).toBeInTheDocument();
+      expect(screen.getByText("Live Sessions")).toBeInTheDocument();
+      expect(screen.getByText("Login")).toBeInTheDocument();
+    });
   });
 });
