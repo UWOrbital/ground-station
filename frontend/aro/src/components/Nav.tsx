b0ea75f6567dcbcd52f75a9dd4e937f925403fb8
Zaid Abdulameer <zabdulam33r@gmail.com>
2026-07-18 22:39:35 -0400

    Wire auth to navbar

--- frontend/aro/src/components/Nav.tsx
+++ frontend/aro/src/components/Nav.tsx
@@ -1,15 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import orbital_logo from "../assets/orbital_logo.png";
 // import { CSS_VARIABLES } from "../utils/themes";
 import { NAVIGATION_LINKS } from "../utils/routes";
+import { useAuth } from "../contexts/AuthContext";
 
 /**
  * @brief Nav component displaying the navigation bar
  * @return tsx element of Nav component
  */
 function Nav() {
-  // TODO: create user auth that checks if the user is logged in
-  const isLoggedIn = false;
+  const { isAuthenticated, loading, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate("/");
+  };
 
   return (
     <nav className="m-7 text-white">
@@ -21,7 +27,9 @@ function Nav() {
       </div>
 
       {/* Navigation Links */}
-      {isLoggedIn ? (
+      {loading ? (
+        <div className="absolute right-14 mt-2 flex space-x-7" />
+      ) : isAuthenticated ? (
         <div className="absolute right-14 mt-2 flex space-x-7">
           {NAVIGATION_LINKS.map((link) => (
             <Link key={link.url} to={link.url} className="mt-1 hover:underline">
@@ -29,10 +37,16 @@ function Nav() {
             </Link>
           ))}
           <div className="border-1 border-white rounded-xl p-1 px-2 hover:bg-white hover:text-black">
-            <Link to="/profile" className="">
+            <Link to="/profile">
               Profile
             </Link>
           </div>
+          <button
+            onClick={handleLogout}
+            className="border-1 rounded-xl p-1 px-2 hover:bg-red-500 hover:text-white cursor-pointer"
+          >
+            Logout
+          </button>
         </div>
       ) : (
         <div className="absolute right-14 mt-2 flex space-x-7">
