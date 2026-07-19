fee3847790685d30b2b73c59ac11133bb622fd3a
Zaid Abdulameer <zabdulam33r@gmail.com>
2026-07-18 22:32:07 -0400

    Fix duplicate keys tab in navbar

--- frontend/aro/src/components/Nav.tsx
+++ frontend/aro/src/components/Nav.tsx
@@ -28,9 +28,6 @@ function Nav() {
               {link.text}
             </Link>
           ))}
-          <Link to="/keys" className="mt-1 hover:underline">
-            Keys
-          </Link>
           <div className="border-1 border-white rounded-xl p-1 px-2 hover:bg-white hover:text-black">
             <Link to="/profile" className="">
               Profile
@@ -44,9 +41,6 @@ function Nav() {
               {link.text}
             </Link>
           ))}
-          <Link to="/keys" className="mt-1 hover:underline">
-            Keys
-          </Link>
           <div className="border-1 border-white rounded-xl p-1 px-2 hover:bg-white hover:text-black">
             <Link to="/login" className="">
               Login
